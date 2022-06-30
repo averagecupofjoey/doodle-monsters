f50b4bce1e012e9ed1a90e7c4e1fe4e7d6e844e6
Joseph Elias <jrelias@outlook.com>
2022-06-30 18:08:17 -0400

    created login and register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Button, Flex, Heading, Input } from '@chakra-ui/react'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
---
+++ pages/login.tsx
@@ -0,0 +1,23 @@
+import { Button, Flex, Heading, Input } from '@chakra-ui/react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const LoginPage = () =>{
+  return (
+
+  <Flex height='100vh' alignItems='center' justifyContent='center'>
+    <Flex direction='column' background='gray.100' p={12} rounded={6}>
+      <Heading mb={6}> Login</Heading>
+      <Input placeholder='Enter Your Username' variant='filled' mb={3} type='email'></Input>
+      <Input placeholder='Enter Your Password' variant='filled' mb={6} type='password'></Input>
+      <Button colorScheme='teal' mb={3}>Log In</Button>
+        <Link href="/register">
+        <a>Register</a>
+      </Link>
+    </Flex>
+  </Flex>
+  )
+
+}
+
+export default LoginPage
