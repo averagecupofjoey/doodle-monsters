5841ca2ec371962866bc3c2f452556561e6c244d
Joseph Elias <jrelias@outlook.com>
2022-07-13 11:43:54 -0400

    implemented pencil width functionality

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,11 +1,28 @@
-import { Grid, ColorPicker } from '@mantine/core';
+import { Grid, ColorPicker, Slider, Container } from '@mantine/core';
 import CanvasDraw from 'react-canvas-draw';
 import { FC, useState } from 'react';
 import { RiDeleteBin6Fill, RiEraserLine } from 'react-icons/ri';
 import { GrFormNextLink } from 'react-icons/gr';
 import { useElementSize } from '@mantine/hooks';
+import { FaPencilRuler } from 'react-icons/fa';
 
 const WHITE = '#ffffff';
+
+// const MARKS = [
+//   { value: 1, label: 'xs' },
+//   { value: 2, label: 'sm' },
+//   { value: 3, label: 'md' },
+//   { value: 4, label: 'lg' },
+//   { value: 5, label: 'xl' },
+// ];
+
+const MARKS = [
+  { value: 0, label: 'xs' },
+  { value: 25, label: 'sm' },
+  { value: 50, label: 'md' },
+  { value: 75, label: 'lg' },
+  { value: 100, label: 'xl' },
+];
 
 const ColWrapper: FC = ({ children }) => {
   return (
@@ -31,6 +48,11 @@ export default function Card({
   const [erase, toggleErase] = useState(false);
   const [nextSelected, updateNextSelected] = useState(false);
 
+  const [value, setValue] = useState(1);
+  const [endValue, setEndValue] = useState(50);
+
+  const [radiusValue, setRadiusValue] = useState(1);
+
   const {
     ref: imageRef,
     width: imageWidth,
@@ -48,7 +70,7 @@ export default function Card({
               <CanvasDraw
                 canvasWidth={imageWidth}
                 canvasHeight={imageHeight}
-                brushRadius={1}
+                brushRadius={radiusValue}
                 brushColor={erase ? WHITE : color}
               />
             )}
@@ -94,16 +116,45 @@ export default function Card({
             </Grid>
           </div>
           <div className='cardBottom'>
-            <ColorPicker
-              value={erase ? WHITE : color}
-              onChange={(color) => {
-                if (erase) {
-                  return;
-                }
-                updateColor(color);
-              }}
-              size='xs'
-            />
+            <div className='colorContainer'>
+              <ColorPicker
+                value={erase ? WHITE : color}
+                onChange={(color) => {
+                  if (erase) {
+                    return;
+                  }
+                  updateColor(color);
+                }}
+                size='xs'
+              />
+            </div>
+            {/* <Slider
+              label={(val) => MARKS.find((mark) => mark.value === val).label}
+              defaultValue={1}
+              min={1}
+              max={5}
+              step={1}
+              marks={MARKS}
+              styles={{ markLabel: { display: 'none' } }}
+            /> */}
+            {/* <Slider
+              label={(val) => MARKS.find((mark) => mark.value === val).label}
+              defaultValue={50}
+              step={25}
+              marks={MARKS}
+              styles={{ markLabel: { display: 'none' } }}
+            /> */}
+            <Container className='sliderContainer'>
+              <FaPencilRuler />
+              <Slider
+                value={value}
+                min={1}
+                max={10}
+                onChange={setValue}
+                // onChangeEnd={setEndValue}
+                onChangeEnd={setRadiusValue}
+              />
+            </Container>
           </div>
         </div>
       </div>
