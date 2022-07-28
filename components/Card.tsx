5c1dacc7efc9bd309258cc58f67c3ed6f141e208
Joseph Elias <jrelias@outlook.com>
2022-07-28 13:18:03 -0400

    added logic to prevent blank spaces for monster name

--- components/Card.tsx
+++ components/Card.tsx
@@ -100,8 +100,13 @@ export default function Card({
 
     //Trim input name to prevent spaces for monster name
     setMonsterName(monsterName.trim());
-    console.log('monster name is:', monsterName.trim());
-    if (monsterName === '') {
+    // console.log(
+    //   'monster name is:',
+    //   monsterName.trim(),
+    //   'length is:',
+    //   monsterName.trim().length
+    // );
+    if (monsterName.trim().length === 0) {
       setOpened(true);
     } else {
       setNextSelected(true);
